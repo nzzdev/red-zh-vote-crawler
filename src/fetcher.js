4f357985903be28da79aeb98ca6d35bede7966fb
Thomas Preusse <tpreusse@users.noreply.github.com>
2015-03-31 10:23:24 +0200

    improve fetcher error handling

--- src/fetcher.js
+++ src/fetcher.js
@@ -12,24 +12,24 @@ var requestWait = 500; // ms
 function get(options) {
   var deferred = Q.defer();
   requestQueue = requestQueue.then(function() {
-      var requestDeferred = Q.defer();
-      console.log('GET', options.url);
-      request.get(options, function(error, response, data) {
-          if(!error && response.statusCode === 200) {
-              deferred.resolve(data);
-          }
-          else {
-              deferred.reject({
-                error: error,
-                response: response,
-                data: data
-              });
-          }
-          setTimeout(function() {
-              requestDeferred.resolve();
-          }, requestWait);
-      });
-      return requestDeferred.promise;
+    var requestDeferred = Q.defer();
+    console.log('GET', options.url);
+    request.get(options, function(error, response, data) {
+      if(!error && response.statusCode === 200) {
+        deferred.resolve(data);
+      }
+      else {
+        deferred.reject({
+          error: error,
+          response: response,
+          data: data
+        });
+      }
+      setTimeout(function() {
+        requestDeferred.resolve();
+      }, requestWait);
+    });
+    return requestDeferred.promise;
   });
   return deferred.promise;
 }
@@ -38,7 +38,12 @@ module.exports.html = function(url) {
   return get({url: url, encoding: null}).then(function(data) {
     return cheerio.load(converter.convert(data).toString());
   }, function(failure) {
-    console.log(failure.response.statusCode);
+    console.log(
+      '=>',
+      (failure.response || {}).statusCode,
+      failure.error,
+      failure.data
+    );
     throw 'failed to fetch ' + url;
   });
 }
