557a2c8864580751f8ef45872da37bb81e0ca53f
Thomas Preusse <tpreusse@users.noreply.github.com>
2015-03-31 15:07:09 +0200

    support colspans and classes

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,12 +2,36 @@ var fetcher = require('./src/fetcher.js');
 var converter = require('./src/converter.js');
 
 module.exports = function(grunt) {
-  grunt.registerTask('fetch', 'test', function() {
+  grunt.registerTask('fetch:test:simple', 'fetches a simple html table', function() {
     var done = this.async();
     var url = 'http://www.wahlen.zh.ch/wahlen/kr2011_medieninfo/viewer.php?menu=listen_kanton';
 
     fetcher.html(url).then(function($) {
       var rows = converter.cheerioTable($, $('table').last());
+      console.log(rows);
+      done();
+    });
+  });
+
+
+  grunt.registerTask('fetch:test:colspan', 'fetches a html table with colspan', function() {
+    var done = this.async();
+    var url = 'http://www.wahlen.zh.ch/wahlen/kr2011_medieninfo/viewer.php?menu=listen_wk&wk=a';
+
+    fetcher.html(url).then(function($) {
+      var rows = converter.cheerioTable($, $('table').last());
+      console.log(rows);
+      done();
+    });
+  });
+
+
+  grunt.registerTask('fetch:test:classes', 'fetches a html table with classified rows', function() {
+    var done = this.async();
+    var url = 'http://www.wahlen.zh.ch/wahlen/rr2015_preview/viewer.php?table=kandkanton';
+
+    fetcher.html(url).then(function($) {
+      var rows = converter.cheerioTable($, $('table').eq(-2)).slice(0, -1);
       console.log(rows);
       done();
     });
--- src/converter.js
+++ src/converter.js
@@ -1,14 +1,34 @@
 var d3 = require('d3');
+var _ = require('lodash');
 
 module.exports.cheerioTable = function($, $table) {
   var rows = [];
-  $table.find('tr').each(function() {
-    var columns = [];
+  $table.find('tr').each(function(i) {
+    var isHeader = i === 0;
+    var columns = [], classes = [];
+
     $(this).find('td').each(function() {
-      columns.push($(this).text());
+      var text = $(this).text();
+      var colspan = $(this).attr('colspan') || 1;
+      for(var i = 1; i <= colspan; i++) {
+        columns.push(text + (i > 1 ? ' ' + i : ''));
+      }
+      classes = classes.concat(($(this).attr('class') || '').split(' '));
     });
+
+    if(isHeader) {
+      columns.push('classes');
+    }
+    else {
+      columns.push(_.uniq(classes).filter(Boolean).join(' '));
+    }
+
     rows.push(columns);
   });
-  var csv = d3.csv.formatRows(rows);
-  return d3.csv.parse(csv);
+
+  var objects = d3.csv.parse(d3.csv.formatRows(rows));
+  objects.forEach(function(object) {
+    object.classes = object.classes.split(' ');
+  });
+  return objects;
 };
